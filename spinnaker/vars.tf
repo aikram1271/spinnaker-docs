7d95cccc4a78b535db1ca2a8aa5a711915cc3a12
Owain Lewis <owain@owainlewis.com>
2017-07-05 19:52:53 +0100

    Adds basic instance stuff

---
+++ spinnaker/vars.tf
@@ -0,0 +1,30 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+
+variable "compartment_ocid" {}
+variable "ssh_public_key" {}
+variable "ssh_private_key" {}
+
+variable "SubnetOCID" {}
+
+variable "AD" {
+    default = "1"
+}
+
+variable "InstanceShape" {
+    default = "VM.Standard1.2"
+}
+
+variable "InstanceOS" {
+    default = "Oracle Linux"
+}
+
+variable "InstanceOSVersion" {
+    default = "7.3"
+}
+
+variable "BootStrapFile" {
+    default = "./userdata/bootstrap"
+}
