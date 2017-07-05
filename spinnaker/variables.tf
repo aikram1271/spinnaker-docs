015cf3712fb7f663beae2bea0d93c13c7a096f85
Owain Lewis <owain@owainlewis.com>
2017-07-05 20:06:15 +0100

    Update variables.tf

--- spinnaker/variables.tf
+++ spinnaker/variables.tf
@@ -1,31 +1,26 @@
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
+variable "tenancy_ocid" {
+}
+variable "user_ocid" {
+}
+variable "fingerprint" {
+}
+variable "private_key_path" {
+}
+variable "compartment_ocid" {
+}
+variable "ssh_public_key" {
+}
+variable "ssh_private_key" {
+}
 
-variable "compartment_ocid" {}
-variable "ssh_public_key" {}
-variable "ssh_private_key" {}
-
-variable "SubnetOCID" {}
-
-# Choose an Availability Domain
+variable "SubnetOCID" {
+}
 variable "AD" {
     default = "1"
 }
-
 variable "InstanceShape" {
-    default = "VM.Standard1.2"
+    default = "VM.Standard1.16"
 }
-
-variable "InstanceOS" {
-    default = "Oracle Linux"
-}
-
-variable "InstanceOSVersion" {
-    default = "7.3"
-}
-
 variable "BootStrapFile" {
     default = "./userdata/bootstrap"
 }
